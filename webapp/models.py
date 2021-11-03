e6a02c0d97286cd6c225c46d64a5c3033bfb9105
sgalway2086 <sgalway2086@gmail.com>
2021-11-03 03:41:32 +0000

    add forum post migration

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    poster_username = models.CharField(max_length=20, null=False, blank=False)
+    post_name = models.CharField(max_length=60, null=False, blank=False)
+    post_content = models.TextField(null=False, blank=False)
+    post_comments = models.TextField(null=False, blank=False)
