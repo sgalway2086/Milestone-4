c8a8f3325a5d3638d24edb63bf8e03b7e0c6cd89
sgalway2086 <sgalway2086@gmail.com>
2021-11-03 03:45:20 +0000

    change display of post items in admin panel

--- webapp/models.py
+++ webapp/models.py
@@ -6,3 +6,6 @@ class Posts(models.Model):
     post_name = models.CharField(max_length=60, null=False, blank=False)
     post_content = models.TextField(null=False, blank=False)
     post_comments = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
