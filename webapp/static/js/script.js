4fb69b1a308c711f8c43ed5945d485feff04e9f6
sgalway2086 <sgalway2086@gmail.com>
2021-11-11 03:22:16 +0000

    add scrolling function to home.html with javascript, using setTimeout functions to keep time

--- webapp/static/js/script.js
+++ webapp/static/js/script.js
@@ -8,3 +8,35 @@ window.onload =function setSlideImage(){
     },5000);
 }
 /* Set positions of box divs before the scroll function is called */
+
+
+function imageScroll(imageScrollSpeed){
+  var scrollTimer = 0;
+  var pause = 0;
+  pause = pause + 1000;
+  for (let i = 0; i < 100; i++){
+    var scrollTimer = scrollTimer + 10;
+    setTimeout(function(){
+        for(let j = 0; j < 3; j++){
+          let intergerCreater = document.getElementsByClassName("scrollBox")[j].style.left;
+          intergerCreater = intergerCreater.replace("%", "");
+          intergerCreater = intergerCreater - 1;
+          document.getElementsByClassName("scrollBox")[j].style.left = intergerCreater + "%";
+      }
+    },scrollTimer);
+  }
+  setTimeout(function(){
+    for(let i = 0; i < 3; i++){
+      let leftStyleChecker = document.getElementsByClassName("scrollBox")[i].style.left;
+      leftStyleChecker = leftStyleChecker.replace("%", "");
+      if(leftStyleChecker <= -100){
+        document.getElementsByClassName("scrollBox")[i].style.left = 200 + "%";
+      }
+    }
+  },pause);
+  setTimeout(function(){
+    imageScroll();
+  },5000);
+}
+
+/* times and creates the scrolling effect of the 3 divs on the main page */
