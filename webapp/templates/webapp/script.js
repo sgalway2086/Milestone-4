d43bd57ec7219c85c15b18d3d498f45b714e9c61
sgalway2086 <sgalway2086@gmail.com>
2021-11-11 02:43:28 +0000

    add code to set boxes locations to script.js

---
+++ webapp/templates/webapp/script.js
@@ -0,0 +1,9 @@
+window.onload =function setSlideImage(){
+    document.getElementById("box1").style.left = 0 + "%";
+    document.getElementById("box2").style.left = 100 + "%";
+    document.getElementById("box3").style.left = 200 + "%";
+    var imageScrollSpeed = 5000;
+    setTimeout(function(){
+      imageScroll();
+    },5000);
+}
